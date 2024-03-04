44a98187e36331bf6858dffe042e53d5dc84e654
GavinAshworth <gavinashworth315@gmail.com>
2024-03-04 09:30:11 -0800

    Made help button functional using js

--- FrontEnd/js/ChannelPage.js
+++ FrontEnd/js/ChannelPage.js
@@ -3,4 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const messageContainer = document.querySelector('.message-container');
     messageContainer.scrollTop = messageContainer.scrollHeight;
     //
+
 });
+
+//function to redirect help button to help page. Right now its just google
+function redirectToPage() {
+    window.location.href = 'https://www.google.com/search?q=google&oq=google+&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIMCAEQIxgnGIAEGIoFMgYIAhBFGDwyBggDEEUYPDIGCAQQRRg8MgYIBRBFGDwyBggGEEUYQTIGCAcQRRhB0gEINjgxOGowajeoAgCwAgA&sourceid=chrome&ie=UTF-8';
+}
