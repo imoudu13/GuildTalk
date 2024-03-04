560c63ae6003cdddfefa6c0c92f6b7238257a1a5
GavinAshworth <gavinashworth315@gmail.com>
2024-03-03 19:03:59 -0800

    Reversed scroll direction on message board on main channel page. Now when page loads it scrolls similar to how a message app should, newest messages are seen first

---
+++ FrontEnd/js/ChannelPage.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //This is code to reverse the scroll direction of the message board
+    const messageContainer = document.querySelector('.message-container');
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+    //
+});
