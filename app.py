08a320147fde220bd6ca5cbd858396b4193e0fc4
GavinAshworth <gavinashworth315@gmail.com>
2024-03-05 21:11:05 -0800

    Added comments

--- app.py
+++ app.py
@@ -4,17 +4,19 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/Home')
+@app.route('/Login')
 def login(): #This is the default page. Below we will have other pages
     return render_template("login.html")
 
 
 @app.route('/Channel')
-def channel(): #This is the default page. Below we will have other pages
+def channel(): #This is the ChannelPage we will send variabls and stuff here to configure
     return render_template("ChannelPage.html")
 
 
 @app.route('/Profile')
-def profile(): #This is the default page. Below we will have other pages
+def profile(): #This is the profile page we will send variables and stuff here to configure
     return render_template("ProfilePage.html")
 
 
