71ca3926866120164c19879335ec6b95c97c4d4d
imoudu13 <imouduibrahim11@gmail.com>
2024-03-05 19:43:05 -0800

    Refactored the app. Css, JS, and images are in the static folder. HTML files are in the templates folder.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template("ProfilePage.html")
+    return render_template("ChannelPage.html")
 
 
 if __name__ == '__main__':
