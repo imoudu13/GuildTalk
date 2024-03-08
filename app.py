a685636a48c347494d2d0bc86a5be9a0d89e03e1
GavinAshworth <gavinashworth315@gmail.com>
2024-03-07 18:01:56 -0800

    Added basic prompt on clicking the create channel button

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from DatabaseConnections import DatabaseConnect
+
+db = DatabaseConnect()
 
 app = Flask(__name__)
 
--- static/js/ChannelPage.js
+++ static/js/ChannelPage.js
@@ -10,3 +10,19 @@ document.addEventListener('DOMContentLoaded', function() {
 function redirectToPage(url) {
     window.location.href = url;
 }
+
+//function to give pop up to user for create channel button
+function createChannel(){
+    let channelName = prompt("Enter a channel name (1-20 Characters)");
+    if(channelName !== null && channelName !== ""){
+        if(channelName.length < 20){
+
+        }
+        else{
+            alert("Channel name too long");
+        }
+    }
+    else{
+        alert("Please insert a channel name");
+    }
+}
