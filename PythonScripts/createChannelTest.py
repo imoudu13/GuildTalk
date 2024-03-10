8a222e8f461d0e68e6e0efa41765addb67443914
GavinAshworth <gavinashworth315@gmail.com>
2024-03-10 11:14:12 -0700

    Fixed unit tests to work with new db

--- PythonScripts/createChannelTest.py
+++ PythonScripts/createChannelTest.py
@@ -1,24 +1,18 @@
 import unittest
-from unittest.mock import patch
 from createChannel import get_channels, add_channel
 
 
 class TestUserFunctions(unittest.TestCase):
-    @patch('app.db')
-    def test_get_channels_for_existing_user(self, mock_db):
+
+    def test_get_channels_for_existing_user(self):
         # Mock the database handler
-        mock_db.retrieve_from_user_channel.return_value = [{"channelName": 'Tech Talk', "id": 1}]
 
         # Test retrieving channels for an existing user
-        expected_channels = [{"channelName": 'Tech Talk', "id": 1}]
-        actual_channels = get_channels('john_doe')
+        expected_channels = ["general", "random"]
+        actual_channels = get_channels('test_user')
         self.assertEqual(expected_channels, actual_channels)
 
-    @patch('app.db')
-    def test_get_channels_for_nonexistent_user(self, mock_db):
-        # Mock the database handler
-        mock_db.retrieve_from_user_channel.return_value = []
-
+    def test_get_channels_for_nonexistent_user(self):
         # Test retrieving channels for a nonexistent user
         nonexistent_user = 'nonexistent_user'
         expected_channels = []
