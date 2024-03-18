a84b9f86c9d7ab82d2eba2194895b1b565b3f894
imoudu13 <imouduibrahim11@gmail.com>
2024-03-17 21:17:45 -0700

    Reformated the file

--- PythonScripts/registerLogin.py
+++ PythonScripts/registerLogin.py
@@ -4,46 +4,49 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 from flask import jsonify
 import app
 
-class RegistrationForm(FlaskForm):
 
+class RegistrationForm(FlaskForm):
     firstname = StringField('Firstname', validators=[DataRequired(), Length(min=2, max=50)], default='')
 
     lastname = StringField('Lastname', validators=[DataRequired(), Length(min=2, max=50)], default='')
 
-    username = StringField('Username', 
+    username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)], default='')
-    
+
     email = StringField('Email',
                         validators=[DataRequired(), Email()], default='')
-    
+
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)], default='')
 
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')], default='')
 
     SubmitField = SubmitField('Sign Up')
 
+
 class LoginForm(FlaskForm):
-    username = StringField('Username', 
-                            validators=[DataRequired()], default='')
-    
+    username = StringField('Username',
+                           validators=[DataRequired()], default='')
+
     password = PasswordField('Password', validators=[DataRequired()], default='')
 
     SubmitField = SubmitField('Login')
 
-class ResetForm(FlaskForm):
 
+class ResetForm(FlaskForm):
     email = StringField('Email',
-                         validators=[DataRequired(), Email()])
-    
+                        validators=[DataRequired(), Email()])
+
     SubmitField = SubmitField('Reset')
+
 
 def get_user(username):
     # Fetch userinfo from username
     user = app.db.retrieve_from_user(username)
     if user is None:
-        return[]
+        return []
     print(user)
     return user
+
 
 def create_user(firstname, lastname, username, email, password):
     # create a new user in the database
@@ -58,10 +61,7 @@ def create_user(firstname, lastname, username, email, password):
     app.db.insert_into_user(user_info)
     return jsonify({'success': True})
 
+
 def validate_username(self, field):
     if User.query.filter_by(username=field.data).first():
         raise ValidationError('Username already in use.')
-
-    
-
-
