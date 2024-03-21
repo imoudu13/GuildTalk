cd457255ffec6ab3d52eb39734f315818c82cc97
GavinAshworth <gavinashworth315@gmail.com>
2024-03-20 20:14:31 -0700

    Delete message function works as expected

--- PythonScripts/deleteMessage.py
+++ PythonScripts/deleteMessage.py
@@ -7,7 +7,7 @@ def deleteMessage(index, channel):
     if selected_channel is None:
         return []
     messages = selected_channel["messages"]
-    messages.remove(index)
+    del messages[index]
     selected_channel["messages"] = messages
     app.db.update_channel(channel, selected_channel)
     return jsonify({'success': False, 'error': 'User not found'})
